90bfc47a2f96a3a3b8035b4acbcc72b7093eb483
Vinesma <otaviocos_13@hotmail.com>
2020-08-08 12:09:09 -0300

    All previous functions can now be done by the base script

--- khinsider/khscraper.py
+++ khinsider/khscraper.py
@@ -1,10 +1,10 @@
-import requests, bs4, time, sys
+import requests, bs4, time, sys, json, os
 
 htmlFile = 'webpage.txt'
 prefix = 'https://downloads.khinsider.com'
 
 print("-- KHINSIDER SCRAPER --")
-print("V. 1.3\n")
+print("V. 1.5\n")
 
 def textPrompt():
     text = sys.stdin.readline()
@@ -22,8 +22,21 @@ def confirmationPrompt(promptText):
 
     return False
 
+def fileExists(filename):
+    if os.path.isfile(filename):
+        return True
+
+    return False
+
+def cleanDir():
+    if fileExists(htmlFile):
+        os.remove(htmlFile)
+    if fileExists('musicData.json'):
+        os.remove('musicData.json')
+    print("\n[i] Cleanup complete!")
+
 def downloadWebpage(link):
-    print("[download] Fetching webpage...")
+    print("[page] Downloading webpage...")
     response = requests.get(link)
     type(response)
 
@@ -44,14 +57,14 @@ def scrapeHrefs(parsedHtml):
     selection = parsedHtml.select('.playlistDownloadSong a')
     type(selection)
 
-    print('[scraper] Scraping list of links from site...')
+    print('[scraper] Scraping list of tracks from site...')
     trackList = []
     for item in selection:
         link = item.get('href')
         musicTitle = item.parent.parent.find('td', class_="clickable-row").a.string
 
         track = {
-            'title' : musicTitle.replace(' ', '_'),
+            'title': musicTitle.replace(' ', '_'),
             'link': '{}{}'.format(prefix, link),
         }
         trackList.append(track)
@@ -59,67 +72,84 @@ def scrapeHrefs(parsedHtml):
 
     return trackList
 
-def downloadTrack(link, title):
-    download = requests.get(link)
-    type(download)
-
-    download.raise_for_status()
-
-    with open('./downloadedTracks/{}.mp3'.format(title), 'wb') as musicFile:
-        musicFile.write(download.content)
-    print(' Done!')
-
 def scrapeSongLinks(trackList):
+    musicList = []
     trackCount = len(trackList)
     count = 1
-    downloadAlong = False
-    sleepTimer = 20 if trackCount < 26 else 15
-
-    if confirmationPrompt('\nDo you wish to download the tracks alongside?'):
-        downloadAlong = True
-        sleepTimer = 10
+    sleepTimer = 25 if trackCount < 26 else 15
 
     timeOfArrival = (sleepTimer * trackCount) / 60
-    print('\n[i] Grabbing download links in {} second intervals.'.format(sleepTimer))
-    print('[i] This is estimated to take {} minutes\n'.format(round(timeOfArrival, 2)))
+    print('\n[link-scraper] Grabbing download links in {} second intervals.'.format(sleepTimer))
+    print('[link-scraper] This is estimated to take {} minutes\n'.format(round(timeOfArrival, 2)))
 
-    with open('./downloadedTracks/linkData.txt', 'w') as linkData:
-        for track in trackList:
-            res = requests.get(track['link'])
-            type(res)
+    for track in trackList:
+        res = requests.get(track['link'])
+        type(res)
 
-            res.raise_for_status()
+        print(' TRACK {} OF {}...'.format(count, trackCount), end='')
 
-            parsedHtml = bs4.BeautifulSoup(res.text, features="html.parser")
-            selection = parsedHtml.find('span', class_="songDownloadLink")
+        res.raise_for_status()
 
-            link = selection.parent['href']
-            musicTitle = track['title']
+        parsedHtml = bs4.BeautifulSoup(res.text, features="html.parser")
+        selection = parsedHtml.find('span', class_="songDownloadLink")
+        music = {
+            'link': selection.parent['href'],
+            'title': track['title'],
+        }
+        musicList.append(music)
+        print(' Done.')
 
-            linkData.write('{} -o {}.mp3\n'.format(link, musicTitle))
+        if count != trackCount:
+            time.sleep(sleepTimer)
+        count += 1
 
-            print(' TRACK {} OF {}'.format(count, trackCount))
-            print(' TITLE : {}'.format(musicTitle))
-            if downloadAlong:
-                print(' Downloading...', end='')
-                downloadTrack(link, musicTitle)
-            print('---------------')
+    with open('musicData.json', 'w') as musicData:
+        musicData.write(json.dumps(musicList))
 
-            if count != trackCount:
-                time.sleep(sleepTimer)
-            count += 1
+    return musicList
+
+def downloadTracks(musicList):
+    trackCount = len(musicList)
+    count = 1
+    sleepTimer = 10
+    print('\n[music] Download starting...\n')
+
+    for music in musicList:
+        download = requests.get(music['link'])
+        type(download)
+
+        print(' TRACK {} OF {}...'.format(count, trackCount))
+        print(' DOWNLOADING: {}'.format(music['title']), end='')
+
+        download.raise_for_status()
+
+        with open('./downloadedTracks/{}.mp3'.format(music['title']), 'wb') as musicFile:
+            musicFile.write(download.content)
+        print(' Done.')
+        print('------------------')
+
+        if count != trackCount:
+            time.sleep(sleepTimer)
+        count += 1
+
+    print('[music] Download complete!')
+    cleanDir()
 
 def main():
-    print("Paste link to download from khinsider: ")
-    link = textPrompt()
-    downloadWebpage(link)
-    html = parseHtml()
-    trackList = scrapeHrefs(html)
-
-    if confirmationPrompt("Do you wish to start fetching links now?"):
-        scrapeSongLinks(trackList)
+    if fileExists('musicData.json') and confirmationPrompt("Found a cache file, download it?"):
+        with open('musicData.json', 'r') as jsonCache:
+            musicList = json.load(jsonCache)
+        downloadTracks(musicList)
     else:
-        print("[i] Aborted by user.")
+        print("Paste link to download from khinsider: ")
+        link = textPrompt()
+        downloadWebpage(link)
+        html = parseHtml()
+        trackList = scrapeHrefs(html)
+        musicList = scrapeSongLinks(trackList)
+
+        if confirmationPrompt("\nDownload music now?"):
+            downloadTracks(musicList)
 
     print('\n[khscraper] Finished!')
 
