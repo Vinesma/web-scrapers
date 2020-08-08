310ddaf8648bbe80430c29eb1557ef5fae3bfacd
Vinesma <otaviocos_13@hotmail.com>
2020-08-08 10:39:27 -0300

    Add option to download files straightaway

--- khinsider/khscraper.py
+++ khinsider/khscraper.py
@@ -4,13 +4,23 @@ htmlFile = 'webpage.txt'
 prefix = 'https://downloads.khinsider.com'
 
 print("-- KHINSIDER SCRAPER --")
-print("V. 1.1\n")
+print("V. 1.3\n")
 
-def handleText():
+def textPrompt():
     text = sys.stdin.readline()
     text = text.rstrip()
 
     return text
+
+def confirmationPrompt(promptText):
+    print(promptText + " (y/n)")
+    text = sys.stdin.readline()
+    text = text.rstrip().lower()
+
+    if text == 'y':
+        return True
+
+    return False
 
 def downloadWebpage(link):
     print("[download] Fetching webpage...")
@@ -20,15 +30,13 @@ def downloadWebpage(link):
     # Raises an exception if the download is unsuccessful
     response.raise_for_status()
 
-    saveFile = open(htmlFile, 'wb')
-    for chunk in response.iter_content(100000):
-        saveFile.write(chunk)
-    saveFile.close()
+    with open(htmlFile, 'wb') as saveFile:
+        for chunk in response.iter_content(100000):
+            saveFile.write(chunk)
 
 def parseHtml():
-    rawHtml = open(htmlFile)
-    parsedHtml = bs4.BeautifulSoup(rawHtml.read(), features="html.parser")
-    rawHtml.close()
+    with open(htmlFile) as rawHtml:
+        parsedHtml = bs4.BeautifulSoup(rawHtml.read(), features="html.parser")
 
     return parsedHtml
 
@@ -51,47 +59,68 @@ def scrapeHrefs(parsedHtml):
 
     return trackList
 
+def downloadTrack(link, title):
+    download = requests.get(link)
+    type(download)
+
+    download.raise_for_status()
+
+    with open('./downloadedTracks/{}.mp3'.format(title), 'wb') as musicFile:
+        musicFile.write(download.content)
+    print(' Done!')
+
 def scrapeSongLinks(trackList):
     trackCount = len(trackList)
     count = 1
+    downloadAlong = False
     sleepTimer = 20 if trackCount < 26 else 15
-    timeOfArrival = (sleepTimer * trackCount) / 60
 
+    if confirmationPrompt('\nDo you wish to download the tracks alongside?'):
+        downloadAlong = True
+        sleepTimer = 10
+
+    timeOfArrival = (sleepTimer * trackCount) / 60
     print('\n[i] Grabbing download links in {} second intervals.'.format(sleepTimer))
     print('[i] This is estimated to take {} minutes\n'.format(round(timeOfArrival, 2)))
-    
-    linkData = open('./downloadedTracks/linkData.txt', 'w')
-    for track in trackList:
-        res = requests.get(track['link'])
-        type(res)
 
-        res.raise_for_status()
+    with open('./downloadedTracks/linkData.txt', 'w') as linkData:
+        for track in trackList:
+            res = requests.get(track['link'])
+            type(res)
 
-        parsedHtml = bs4.BeautifulSoup(res.text, features="html.parser")
-        selection = parsedHtml.find('span', class_="songDownloadLink")
+            res.raise_for_status()
 
-        link = selection.parent['href']
-        musicTitle = track['title']
+            parsedHtml = bs4.BeautifulSoup(res.text, features="html.parser")
+            selection = parsedHtml.find('span', class_="songDownloadLink")
 
-        linkData.write('{} -o {}.mp3\n'.format(link, musicTitle))
+            link = selection.parent['href']
+            musicTitle = track['title']
 
-        print(' TRACK {} OF {}'.format(count, trackCount))
-        print(' TITLE : {}'.format(musicTitle))
-        print('---------------')
-        if count != trackCount:
-            time.sleep(sleepTimer)
-        count += 1
+            linkData.write('{} -o {}.mp3\n'.format(link, musicTitle))
 
-    linkData.close()
+            print(' TRACK {} OF {}'.format(count, trackCount))
+            print(' TITLE : {}'.format(musicTitle))
+            if downloadAlong:
+                print(' Downloading...', end='')
+                downloadTrack(link, musicTitle)
+            print('---------------')
+
+            if count != trackCount:
+                time.sleep(sleepTimer)
+            count += 1
 
 def main():
     print("Paste link to download from khinsider: ")
-    link = handleText()
+    link = textPrompt()
     downloadWebpage(link)
     html = parseHtml()
     trackList = scrapeHrefs(html)
-    scrapeSongLinks(trackList)
-    
+
+    if confirmationPrompt("Do you wish to start fetching links now?"):
+        scrapeSongLinks(trackList)
+    else:
+        print("[i] Aborted by user.")
+
     print('\n[khscraper] Finished!')
 
 main()
